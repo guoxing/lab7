bc1c12815fa4e4f29aa5a6ee4d3778db127203fb
Guoxing Li <guoxing@signalfuse.com>
2014-02-19 14:23:39 -0800

    lab7

--- routes/project.js
+++ routes/project.js
@@ -5,17 +5,31 @@ exports.projectInfo = function(req, res) { 
 
   // query for the specific project and
   // call the following callback
+  models.Project.findById(projectID, afterQuery);
 
-  function afterQuery(err, projects) {
+  function afterQuery(err, project) {
     if(err) console.log(err);
-    res.json(projects[0]);
+    res.json(project);
   }
 }
 
 exports.addProject = function(req, res) {
   var form_data = req.body;
   console.log(form_data);
-
+  var project = new models.Project({
+    title: form_data.project_title,
+    date: form_data.date,
+    summary: form_data.summary,
+    image: form_data.image_url
+  });
+  project.save(function (err) {
+    if (err) {
+      console.log(err);
+      res.send(400);
+      return;
+    }
+    res.send(200);
+  });
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
 }
@@ -25,4 +39,12 @@ exports.deleteProject = function(req, res) {
 
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
+  models.Project.findByIdAndRemove(projectID, function(err) {
+    if (err) {
+      console.log(err);
+      res.send(400);
+      return;
+    }
+    res.send(200);
+  });
 }
